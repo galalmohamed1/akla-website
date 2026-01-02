d1b62596c5c07977dfc88feb08ee4b750d773393
galalmohamed1 <gm3308769@gmail.com>
2026-01-02 15:55:12 +0200

    Change the XML to Fetch

--- js/script.js
+++ js/script.js
@@ -1,42 +1,34 @@
 let Value;
-let recipe = [];
 let btn = document.querySelector(".btton");
-btn.addEventListener("click",function(){
+
+
+btn.addEventListener("click", function () {
   Value = document.querySelector(".form-control").value;
-  getRecipe(Value);
-})
-function getRecipe(Value) {
-  let p = new XMLHttpRequest();
-  p.open("GET", `https://forkify-api.herokuapp.com/api/search?q=${Value}`);
-  p.send();
+  getPizza(Value);
+});
 
-  p.responseType = "json";
-
-  p.addEventListener("readystatechange", function () {
-    if (p.readyState === 4) {
-      recipe = p.response.recipes;
-      for (let i = 0; i < recipe.length; i++) {
-        displayData();
-      }
-    }
-  });
+async function getPizza(value) {
+  let response = await fetch(`https://forkify-api.herokuapp.com/api/search?q=${value}`);
+  if(response.ok){
+    let data = await response.json();
+    let AllRecipe = data.recipes;
+    displayData(AllRecipe);
+  }
 }
-
-function displayData() {
+function displayData(AllRecipe) {
   let cartona = "";
-  for (let i = 0; i < recipe.length; i++) {
-    let titel = recipe[i].title;
+  for (let i = 0; i < AllRecipe.length; i++) {
     cartona += `<div class="col-md-3">
                         <div class="card">
                             <img class="card-img-top" src="${
-                              recipe[i].image_url
+                              AllRecipe[i].image_url
                             }" alt="Title">
                             <div class="card-body">
-                                <h4 class="card-title">${recipe[i].title
+                                <h4 class="card-title">${AllRecipe[i].title
                                   .split(" ", 2)
                                   .join(" ")}</h4>
-                                <a target="_blank" class="btn btn-primary" href="${
-                                  recipe[i].source_url
+                                <a target="_blank" class="btn btn-primary w-100" href="${
+                                  AllRecipe[i].source_url
                                 }">text</a>
                             </div>
                         </div>
